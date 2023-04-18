58260625dbe8a8f47e3d33ab967f594eaf40c173
TiagoMLucio <tiagolucio100@gmail.com>
2023-04-18 06:12:50 -0300

    :tada: add: inner join in jogo_comprado

--- server/src/modules/bought_games/infra/database/repositories/BoughtGames.ts
+++ server/src/modules/bought_games/infra/database/repositories/BoughtGames.ts
@@ -1,7 +1,6 @@
 import IBoughtGamesRepository from '@modules/bought_games/repositories/IBoughtGamesRepository';
 import ICreateBoughtGameDTO from '@modules/bought_games/dtos/ICreateBoughtGameDTO';
 
-
 import BoughtGameSchema from '@modules/bought_games/infra/database/schemas/BoughtGames';
 import MariaDB from '@config/mariadb';
 
@@ -19,17 +18,15 @@ export default class BoughtGamesRepository implements IBoughtGamesRepository {
       const conn = await this.mariadb.getConnection();
       await this.mariadb.query(
         `INSERT INTO jogo_comprado (conta_id, jogo_id, data_compra, tempo_jogado, curtida)
-         VALUES ('${conta_id}', '${jogo_id}', ${data_compra}, ${tempo_jogado}, '${curtida}');`,
+         VALUES ('${conta_id}', '${jogo_id}', '${data_compra}', ${tempo_jogado}, '${curtida}');`,
       );
       conn.end();
     }
 
-
     public async findBoughtByMe(conta_id: string): Promise<BoughtGameSchema[]> {
       const conn = await this.mariadb.getConnection();
-      const game = await conn.query(`SELECT * FROM jogo_comprado WHERE conta_id = "${conta_id}";`);
+      const game = await conn.query(`SELECT jogo_comprado.data_compra, jogo_comprado.tempo_jogado, jogo_comprado.curtida, jogo.* FROM jogo_comprado INNER JOIN jogo ON jogo_comprado.jogo_id = jogo.jogo_id WHERE jogo_comprado.conta_id = '${conta_id}';`);
       conn.end();
       return game;
     }
-
 }
